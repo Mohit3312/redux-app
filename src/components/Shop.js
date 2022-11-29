1f22e2e11ec6d5b38a655bc4df360210b1393b73
Mohit3312 <mohit3312@gmail.com>
2022-11-29 12:15:43 +0530

    Initial Commit - Setup a React Application for Redux

---
+++ src/components/Shop.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Shop = () => {
+  return (
+    <div className="container my-3 text-center">
+      <h2>Deposit/Withdraw Money</h2>
+      <button className="btn btn-primary btn-sm mx-2">-</button>
+      Update Balance
+      <button className="btn btn-primary btn-sm mx-2">+</button>
+    </div>
+  );
+};
+
+export default Shop;
